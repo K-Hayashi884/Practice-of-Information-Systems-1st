f735149c567ffd04ee26f10f751e7f0d6afb0046
asamakei <kei.asama@gmail.com>
2023-05-02 08:49:26 +0900

    Itemの追加処理を作成

--- test_asama/database_test.py
+++ test_asama/database_test.py
@@ -1,9 +1,56 @@
-import sqlite3
+import sqlalchemy
+import sqlalchemy.orm
+from db_Init import Store, Item, Recipe, Ingredient, Handling, Need
 
-def connect():
-    return sqlite3.connect('test_asama/TestDB.db')
+def get_session():
+    engine = sqlalchemy.create_engine('sqlite:///db.sqlite3', echo=True)
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+    return session
 
-def searchByStore():
-    db = connect()
-    res = db.execute("SELECT * FROM store")
-    print(res)
+def get_store(id: int = -1, name: str = ""):
+    session = get_session()
+    if id == -1 and name == "":
+        stores = session.query(Store).all()
+    elif id == -1:
+        stores = session.query(Store).filter(Store.name==name)
+    else:
+        stores = session.query(Store).filter(Store.id==id)
+    
+    result: list[tuple[int, str, float, float, str]] = []
+    for store in stores:
+        result.append((store.id, store.name, store.latitude, store.longitude, store.flyer_url))
+
+    return stores
+
+def get_item(id: int = -1, name: str = ""):
+    session = get_session()
+    if id == -1 and name == "":
+        items = session.query(Item).all()
+    elif id == -1:
+        items = session.query(Item).filter(Item.name==name)
+    else:
+        items = session.query(Item).filter(Item.id==id)
+    
+    result: list[tuple[int, str]] = []
+    for item in items:
+        result.append((item.id, item.name))
+
+    return result
+
+def add_item(name: str):
+    session = get_session()
+    item = Item(name=name)
+    session.add(item)
+    session.commit()
+    return item.id
+
+def add_handling(store_id: int, item_id: int):
+    session = get_session()
+    handling = Handling(store_id=store_id, item_id=item_id)
+    session.add(handling)
+    session.commit()
+
+def clear_item():
+    session = get_session()
+    session.query(Item).delete()
--- test_asama/usecase_test.py
+++ test_asama/usecase_test.py
@@ -5,9 +5,9 @@ class storeRequest:
         self.id = id
 
 class storeResponse:
-    test: int
-    def __init__(self, test):
-        self.test = test
+    store_names: list
+    def __init__(self, store_name_list):
+        self.store_names = store_name_list
 
 class itemRequest:
     id: int
@@ -20,12 +20,42 @@ class itemResponse:
         self.test = test
 
 def searchByStore(request: storeRequest) -> storeResponse:
-    result = database.searchByStore()
-    return storeResponse(
-        test = request.id
-    )
+    result = database.searchByStore(request.id)
+    response = storeResponse(result)
+    return response
 
 def searchByItem(request: itemRequest) -> itemResponse:
     return itemResponse(
         test = request.id
     )
+
+def get_dict_store_name_to_id():
+    stores = database.get_store()
+    store_name_to_id: dict[str, int] = {}
+    for store in stores:
+        store_name_to_id[store[1]] = store[0]
+    return store_name_to_id
+
+def get_dict_item_name_to_id():
+    items = database.get_item()
+    item_name_to_id: dict[str, int] = {}
+    for item in items:
+        item_name_to_id[item[1]] = item[0]
+    return item_name_to_id
+
+def add_item_by_name(handlings:list[tuple[str, str, int]]):
+    store_name_to_id = get_dict_store_name_to_id()
+    item_name_to_id = get_dict_item_name_to_id()
+
+    for handling in handlings:
+        store_id = store_name_to_id.get(handling[0], -1)
+        item_id = item_name_to_id.get(handling[1], -1)
+        if store_id < 0:
+            continue
+        if item_id < 0:
+            item_id = database.add_item(handling[1])
+            item_name_to_id[handling[1]] = item_id
+        database.add_handling(store_id, item_id)
+
+def clear_item():
+    database.clear_item()

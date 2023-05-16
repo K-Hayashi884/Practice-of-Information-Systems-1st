01e5bc2d5d6f12ca2970af8e04f30f2fb30bf2ea
Nmasaaa <nojima.masaya.82c@st.kyoto-u.ac.jp>
2023-05-16 15:09:19 +0900

    タプルによる出力に変更

--- get_pdf.py
+++ get_pdf.py
@@ -1,112 +1,135 @@
-# 公式サイトからpdfリンク一覧取得
-def get_urls():
-  import requests
-  from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
+import calendar
+#ユーザエージェント変更
+ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+headers = {'User-Agent': ua}
 
-  url = "https://tokubai.co.jp/%E3%83%A9%E3%82%A4%E3%83%95/9470/leaflets/41074828?from=leaflet_navigation&origin_shop_id=9470"
+## 1-2.店ごとに取得する条件が異なるため、`link1_to_link2(link1)`という関数を作成。
+def link1_to_link2(link1_list):
+    """
+    リスト`link1_list`に含まれる適切なチラシの階層2リンク(文字列)のリスト(`link2_list`)を返す
+    """
+    link2_list = [] #最終的には取得日に乗っているチラシのリンク(link2)一覧が格納される
+    for link1 in link1_list:
+        if "%E3%83%A9%E3%82%A4%E3%83%95" in link1[0]: #ライフの場合
+            r = requests.get(link1[0],headers=headers)
+            soup = BeautifulSoup(r.text,"html.parser")
+            elements = soup.find_all(class_="image_element")
+            for element in elements:
+                link = element.get("href")
+                l = "https://tokubai.co.jp"
+                date = element.find(class_="description").string
+                if date.count("月") == 1: #チラシ掲載期間が月を跨がない場合 例：/n2023年4月2〜4日まで/n -> 2023,4,2,4
+                    date = date[1:-1].replace("月",",").replace("〜",",").replace("日まで",",").replace("年",",")
+                    dates = date.split(",")
+                    period = int(dates[3]) - int(dates[2])
+                else: #チラシ掲載期間が月を跨ぐ場合　例：/n2023年4月29日〜5月2日まで/n  -> 2023,4,29,5,2
+                    date = date[1:-1].replace("月",",").replace("日〜",",").replace("日まで",",").replace("年",",")
+                    dates = date.split(",")
+                    period = int(dates[4]) - int(dates[2]) + calendar.monthrange(2023, int(dates[1])+1)[1]
+                if period < 6: #ライフでは一般的に掲載期間が6日以上のチラシは取得したくないチラシなので
+                    link2_list.append((l+link,link1[1]))
+        elif "%E3%82%B0%E3%83%AC%E3%83%BC%E3%82%B9%E3%81%9F%E3%81%AA%E3%81%8B" in link1[0]: #グレースたなかの場合
+            r = requests.get(link1[0],headers=headers)
+            soup = BeautifulSoup(r.text,"html.parser")
+            elements = soup.find_all(class_="image_element")
+            for element in elements:
+                link = element.get("href")
+                l = "https://tokubai.co.jp"
+                date = element.find(class_="description").string
+                link2_list.append((l+link,link1[1]))
+    return link2_list
 
+## 1-3.`link2_list`には最新ではないチラシも含まれるため、関数`update_urllist(link2_list)`を作成。
+def update_url_list(link2_list):
+    """
+    `link2_list`に含まれる最新でないURLを、./urls.txtを利用し削除
+    昨日まで存在しなかったチラシのリンクのリストを返す
+    """
+    new_link2_list = []
+    with open('urls.txt',"r") as f: #urls.txtの中には昨日使用したlink2リンクの一覧がある
+        lines = f.readlines()
+        old_link2_list = [line.replace("¥n","").replace("\n","") for line in lines] #改行文字削除 怖かったので\と¥を書いた
+    for link2 in link2_list:
+        if link2[0] in old_link2_list: #昨日使用したリンクは無視
+            continue
+        else: #昨日なかったリンクは欲しいのでnew_link2_listに格納
+            new_link2_list.append(link2)
+            
+    urls_new = map(lambda x: x[0] + "\n", link2_list)
+    #link2_listの内容をurls.txtに保存
+    with open('urls.txt', 'w', encoding='utf-8') as f:
+        f.writelines(urls_new)
+    return new_link2_list
 
-  html = requests.get(url)
-  soup = BeautifulSoup(html.text, 'html.parser')
+## 1-4.`get_urls()`で階層2のリンクを階層3のリンクに変換
+def get_url(new_link2_list):
+  """
+  link2をlink3(.jpg)に変換する
+  """
+  link3_list = []
+  for new_link2 in new_link2_list:
+    r = requests.get(new_link2[0],headers=headers)
+    soup = BeautifulSoup(r.text,"html.parser")
+    link3_url = soup.find('img',class_='leaflet').get('src').split("?")[0]
+    link3_list.append((link3_url,new_link2[1])) 
+  return link3_list
 
-  # ★以下は実際のHTML構成に合わせて実装する
-  flyer_list = soup.find_all('table')
-  url_list = []
-  for flyer in flyer_list:
-    # 日付
-    date = flyer.find('div', {'class': 'sale'}).find('a').get_text(strip=True).replace(' ', '').replace('（', '(').replace('）', ')')
+## 2-1. type2の店リストから特売商品のリストを取得
+def get_bargains(link1_list):
+    """
+    type2の店のlink1リストから、特売商品のリストを取得
+    """
+    sales_list = []
+    for link1 in link1_list:
+        l = link1[0]
+        r = requests.get(l,headers=headers)
+        soup = BeautifulSoup(r.text,"html.parser")
+        if "%E3%82%B3%E3%83%AC%E3%83%A2/187995" in l: #コレモ出町柳の場合
+            l = "https://tokubai.co.jp/%E3%82%B3%E3%83%AC%E3%83%A2"
+            if len(soup.find_all(class_="corner_more_link_wrapper")) == 1:
+                link = soup.find(class_="corner_more_link")
+                l = l + link.get("href")
+                r = requests.get(l,headers=headers)
+                soup = BeautifulSoup(r.text,"html.parser")
+            elements = soup.find_all(class_="name hoverable_link")
+            sales_list.append(([element.contents[0][1:-1] for element in elements],link1[1]))
 
-    # PDF
-    url_info = {}
-    url_info['date'] = date
-    url_info['url'] = flyer.find('a', {'title': 'PDF'})['href']
-    url_list.append(url_info)
+        elif "%E5%A4%A7%E5%9B%BD%E5%B1%8B/14801" in l: #大黒屋北白川の場合
+            elements = soup.find_all(class_="name hoverable_link")
+            sales_list.append(([element.contents[0][1:-1] for element in elements],link1[1]))
 
-  return url_list
-"""
-# 未解析のチラシURLを取得
-def get_new_urls(url_list):
-  # urls.txt読込
-  old_url_list = []
-  with open('/urls.txt', 'r') as f:
-    old_url_list = f.read().splitlines()
+        elif "%E3%83%95%E3%83%AC%E3%82%B9%E3%82%B3/4244" in l: #フレスコ川端の場合
+            elements = soup.find_all(class_="name hoverable_link")
+            sales_list.append(([element.contents[0][1:-1] for element in elements],link1[1]))
 
-  new_url_list = []
-  urls_text = []
-  count = 0
-  for url_info in url_list:
-    urls_text.append(url_info['url'] + '\n')
+        elif "%E3%82%A4%E3%82%BA%E3%83%9F%E3%83%A4/9148" in l: #イズミヤ高野の場合
+            elements = soup.find_all(class_="name hoverable_link")
+            sales_list.append(([element.contents[0][1:-1] for element in elements],link1[1]))
+    return sales_list
 
-    if url_info['url'] not in old_url_list:
-      # 新規
-      url_info['number'] = count
-      new_url_list.append(url_info)
-      count += 1
+#　使用するのは、(店名,URL,(if type1 then 0 else 1))というタプルのリスト
+#　これを受け取った後、type1の店リストとtype2の店リストの2つに振り分け、それぞれ
 
-  # 今回のURL一覧をurls.txtに書込
-  f = open('/urls.txt', 'w')
-  f.writelines(urls_text)
-  f.close()
+input_list = []
+input_list.append(("グレースたなか","https://tokubai.co.jp/%E3%82%B0%E3%83%AC%E3%83%BC%E3%82%B9%E3%81%9F%E3%81%AA%E3%81%8B/12265",0))
+input_list.append(("ライフ北白川","https://tokubai.co.jp/%E3%83%A9%E3%82%A4%E3%83%95/9470",0))
+input_list.append(("大国屋白川店","https://tokubai.co.jp/%E5%A4%A7%E5%9B%BD%E5%B1%8B/14801",1))
+input_list.append(("コレモ出町柳","https://tokubai.co.jp/%E3%82%B3%E3%83%AC%E3%83%A2/187995",1))
+input_list.append(("フレスコ河原町丸太町","https://tokubai.co.jp/%E3%83%95%E3%83%AC%E3%82%B9%E3%82%B3/4244",1))
+input_list.append(("イズミヤ高野店","https://tokubai.co.jp/%E3%82%A4%E3%82%BA%E3%83%9F%E3%83%A4/9148",1))
 
-  return new_url_list
+type1_link1_list = []
+type2_link1_list = []
+for t in input_list:
+    if t[2] == 0:
+        type1_link1_list.append((t[1],t[0]))
+    else:
+        type2_link1_list.append((t[1],t[0]))
 
-# 新規のチラシPDFをDL
-def dl_pdfs(new_url_list):
-  import urllib.request
-  import time
+link2_list = link1_to_link2(type1_link1_list)
+new_link2_list = update_url_list(link2_list)
 
-  pdf_list = []
-  for url_info in new_url_list:
-    file_name = f'./pdf/{url_info["number"]}.pdf'
-    urllib.request.urlretrieve(url_info['url'], file_name)
-    url_info['pdf_path'] = file_name
-    # サーバーに負荷がかからないように時間をおく
-    time.sleep(2)
-
-    pdf_list.append(url_info)
-
-  return pdf_list
-
-# PDFをJPGに変換
-def pdf_to_jpeg(path):
-  import os
-  from pathlib import Path
-  from pdf2image import convert_from_path
-
-  # poppler/binを環境変数PATHに追加する
-  poppler_dir = '/lib/poppler/bin'
-  os.environ['PATH'] += os.pathsep + str(poppler_dir)
-
-  image_paths = []
-
-  pdf_path = Path(path)
-  # PDF -> Image に変換（150dpi）
-  pages = convert_from_path(str(pdf_path), 150)
-
-  # 画像ファイルを１ページずつ保存
-  image_dir = Path('./jpg')
-  for i, page in enumerate(pages):
-    file_name = pdf_path.stem + '_{:02d}'.format(i + 1) + '.jpeg'
-    image_path = image_dir / file_name
-    # JPEGで保存
-    page.save(str(image_path), 'JPEG')
-    image_paths.append(image_path)
-
-  return image_paths
-
-### FlyerOCR ###
-import shutil
-import os
-os.makedirs('pdf/', exist_ok=True)
-os.makedirs('jpg/', exist_ok=True)
-
-url_list = get_urls()
-new_url_list = get_new_urls(url_list)
-pdf_list = dl_pdfs(new_url_list)
-jpg_list = pdfs_to_jpeg(pdf_list)
-results = get_target_flyers(jpg_list)
-slack_notice(results)
-
-shutil.rmtree('pdf/')
-shutil.rmtree('jpg/')
-"""
+type1_link3_list = get_url(new_link2_list)
+type2_string_list = get_bargains(type2_link1_list)
